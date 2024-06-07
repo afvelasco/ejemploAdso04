c0ee721c566427b5d5ba744adc04d16b461141de
Andres F Velasco <afvelasco@gmail.com>
2024-06-07 10:33:33 -0500

    Se agrega modificar usuario

--- models/usuarios.py
+++ models/usuarios.py
@@ -32,14 +32,44 @@ class Usuarios:
     def agregar(self,nuevo):
         ahora = datetime.now()
         tiempo = ahora.strftime("%Y%m%d%H%M%S")
+        cifrada = hashlib.sha512(nuevo[4].encode("utf-8")).hexdigest()
         if nuevo[3].filename != "":
             nombre,extension = os.path.splitext(nuevo[3].filename)
             nuevonombre = "U" + tiempo + extension
             nuevo[3].save("uploads/"+nuevonombre)
-        sql = f"INSERT INTO usuarios (id_usuario,contrasena,nombre,rol,foto) VALUES ('{nuevo[0]}','{nuevo[4]}','{nuevo[1]}',{nuevo[2]},'{nuevonombre}')"
+        sql = f"INSERT INTO usuarios (id_usuario,contrasena,nombre,rol,foto) VALUES ('{nuevo[0]}','{cifrada}','{nuevo[1]}',{nuevo[2]},'{nuevonombre}')"
         self.cursor.execute(sql)
         self.mi_DB.commit()
 
+    def consulta_usuario(self,id):
+        sql = f"SELECT * FROM usuarios WHERE id_usuario='{id}'"
+        self.cursor.execute(sql)
+        resultado = self.cursor.fetchall()
+        return resultado
+        
+    def modificar(self,nuevo):
+        sql = f"UPDATE usuarios SET nombre='{nuevo[1]}',rol={nuevo[2]} WHERE id_usuario='{nuevo[0]}'"
+        self.cursor.execute(sql)
+        self.mi_DB.commit()
+        if nuevo[4]!="":
+            cifrada = hashlib.sha512(nuevo[4].encode("utf-8")).hexdigest()
+            sql = f"UPDATE usuarios SET contrasena='{nuevo[4]}' WHERE id_usuario='{nuevo[0]}'"
+            self.cursor.execute(sql)
+            self.mi_DB.commit()
+        if nuevo[3].filename != "":
+            ahora = datetime.now()
+            tiempo = ahora.strftime("%Y%m%d%H%M%S")
+            nombre,extension = os.path.splitext(nuevo[3].filename)
+            nuevonombre = "U" + tiempo + extension
+            nuevo[3].save("uploads/"+nuevonombre)
+            sql=f"SELECT foto FROM usuarios WHERE id_usuario='{nuevo[0]}'"
+            self.cursor.execute(sql)
+            foto_vieja = self.cursor.fetchall()[0][0]
+            if foto_vieja!="":
+                os.remove(os.path.join(self.app.config['CARPETAU'],foto_vieja))
+            sql = f"UPDATE usuarios SET foto='{nuevonombre}' WHERE id_usuario='{nuevo[0]}'"
+            self.cursor.execute(sql)
+            self.mi_DB.commit()
 
     
 usuarios = Usuarios(app,mi_DB)
--- routes/usuarios.py
+++ routes/usuarios.py
@@ -40,9 +40,36 @@ def guardausuario():
     confir = request.form['conficontra']
     if not usuarios.buscar(id):
         if contra==confir:
-            usuarios.agregar([id,nombre,rol,foto,contra,confir])
+            usuarios.agregar([id,nombre,rol,foto,contra])
             return redirect('/principal')
         else:
             return render_template("agregarusuario.html",msg="Constraseñas no coinciden")    
     else:
         return render_template("agregarusuario.html",msg="Id Usuario ya existe")
+
+@app.route('/editausuario/<id>/<err>')
+def editausuario(id,err=0):
+    if session.get('login') == True:
+        usuario = usuarios.consulta_usuario(id)
+        if err==0:
+            msg=''
+        else:
+            msg='Contraseñas no coinciden'
+        return render_template("editausuario.html",usu=usuario[0],msg=msg)
+    else:
+        return redirect("/")
+
+@app.route('/actualizausuario', methods=['POST'])
+def actualizausuario():
+    id = request.form['id']
+    nombre = request.form['nombre']
+    rol = request.form['rol']
+    foto = request.files['foto']
+    contra = request.form['contra']
+    confir = request.form['confir']
+    if contra==confir:
+        usuarios.modificar([id,nombre,rol,foto,contra,confir])
+        return redirect('/principal')
+    else:
+        return redirect(f"/editausuario/{id}/1")
+    
